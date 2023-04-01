849146d8808af5ac12af210b7b613146950faaba
Rupeshpal <palrupesh292@gmail.com>
2023-04-01 18:49:38 +0545

    second commit

---
+++ crud-app/src/Component/CodeForInterview.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CodeForInterview() {
+    return (
+        <>
+            <h2>This home page section</h2>
+
+        </>
+    )
+}
+
+export default CodeForInterview
